8792007ee393996fca2bee49ac81a9edcc1cc599
Jonathan Dumlao <dumlao.jonathan.g.5165@gmail.com>
2023-12-03 10:08:18 +0800

    style: change like button to like icon

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -19,20 +19,32 @@ const LikeButton = ({ props, buttonState }: LikeButtonProps) => {
   const [isLoading, startTransition] = useTransition();
 
   return (
-    <Button
-      className="flex items-center gap-2"
+    // <Button
+    //   className="flex items-center gap-2"
+    //   disabled={buttonState?.liked}
+    //   onClick={() => {
+    //     startTransition(async () => {
+    //       await likePost(props.id, props.userid);
+    //     });
+    //   }}
+    // >
+    //   {isLoading && <Loader2 className="animate-spin" />}
+    //   {!isLoading && <BiLike />}
+
+    //   {buttonState?.liked ? "Liked" : "Like"}
+    // </Button>
+
+    <BiLike
+      className={`w-[30px] h-[30px] cursor-pointer ${
+        buttonState?.liked && "text-blue-500"
+      }`}
       disabled={buttonState?.liked}
       onClick={() => {
         startTransition(async () => {
           await likePost(props.id, props.userid);
         });
       }}
-    >
-      {isLoading && <Loader2 className="animate-spin" />}
-      {!isLoading && <BiLike />}
-
-      {buttonState?.liked ? "Liked" : "Like"}
-    </Button>
+    ></BiLike>
   );
 };
 
