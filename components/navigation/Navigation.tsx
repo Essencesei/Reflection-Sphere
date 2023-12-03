55c6ec6f14469f3a042d843ccbe2f2e14033ac34
Jonathan Dumlao <dumlao.jonathan.g.5165@gmail.com>
2023-12-03 15:25:57 +0800

    style: change dialog height

---
+++ components/navigation/Navigation.tsx
@@ -0,0 +1,120 @@
+"use client";
+import { Session } from "next-auth";
+import { signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+import React from "react";
+import CredButton from "../CredButton";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
+
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+} from "../ui/dialog";
+import CreatePost from "../create-post/CreatePost";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+type NavigationProps = {
+  session: Session | null;
+};
+
+const Navigation = ({ session }: NavigationProps) => {
+  return (
+    <div className="flex items-center w-full p-2 justify-between fixed  shadow  z-50 bg-background ">
+      <div>
+        <Image
+          src={"/REFLECTION SPHERE.png"}
+          alt={""}
+          width={50}
+          height={50}
+        ></Image>
+      </div>
+      {session && (
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/feed" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Feed
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href={"/mypage"} legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Posts
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <NavigationMenuLink
+                    className={`${navigationMenuTriggerStyle()} cursor-pointer`}
+                  >
+                    Create
+                  </NavigationMenuLink>
+                </DialogTrigger>
+                <CreatePost />
+              </Dialog>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      )}
+      {session && (
+        <div>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage
+                  src={session.user.image as string}
+                  alt={session.user.name as string}
+                ></AvatarImage>
+                <AvatarFallback>
+                  {session.user.name?.slice(0, 1)}
+                </AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>
+                Signed in as {session.user.name}
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                <Link href={"/feed"}>Feed</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href={"/mypage"}>Posts</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => signOut({ callbackUrl: "/" })}>
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navigation;
